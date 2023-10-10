e60349fda058c8747d99efba1db8b6bc64244c49
DanielAyamba <danielayamba05@gmail.com>
2023-10-10 01:00:51 +0000

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,17 +4,13 @@
  * main - program that prints numbers in base 10 starting from 0
  * Return: Always 0.
  */
-
-int main(void)
+	int main(void)
 {
-    int number;
-
-    for (number = 0; number <= 9; number++)
+    	int number;
+    	for (number = 0; number <= 9; number++)
     {
         putchar(number + '0');
     }
-
-    putchar('\n');
-
-    return (0);
+    		putchar('\n');
+    	return (0);
 }
