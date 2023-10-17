5f98f4032b2f98d2b4abee39d31e0c42220dacca
DanielAyamba <danielayamba05@gmail.com>
2023-10-17 00:27:53 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - function name
+ * @str: function paameter
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i - 0; str[] != '\0' i++)
+_putchar (str[i]);
+_puchar('\n');
+}
