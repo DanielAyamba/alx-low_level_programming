d98599015c272b23d5b5c26e6999d5d8a6d45b46
DanielAyamba <danielayamba05@gmail.com>
2023-10-17 23:10:50 +0000

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - the function name
+ * @str: function parameter
+ */
+
+void puts_half(char *str)
+{
+int i;
+int n;
+int count = 0;
+
+for (i = 0; str[i] != '\0'; i++)
+count++;
+n = (count - 1) / 2;
+for (i = n + 1; str[i] != '\n'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
