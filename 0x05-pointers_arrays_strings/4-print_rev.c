c3cccce151b9c3f8bda55f93bbddd90ff2b8fc21
DanielAyamba <danielayamba05@gmail.com>
2023-10-17 01:14:39 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_new - the function name
+ * @s: function parameter
+ */
+
+void print_new(char *s)
+
+{
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\n';
+count++
+
+for (i = count - 1; i >= 0; i--)
+_putchar (s[i]);
+_putchar ('\n');
+}
