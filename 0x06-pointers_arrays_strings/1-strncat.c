e15530a12e0c7dc55a7219be14f5e96fe4751999
DanielAyamba <danielayamba05@gmail.com>
2023-10-19 20:53:43 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-  * _strncat  - function name
-  * @dest: function parameter one
-  * @src: Function parameter two
-  *@n: function parameter three
-  * Return: dest
-  */
+ * _strncat  - function name
+ * @dest: function parameter one
+ * @src: Function parameter two
+ * @n: function parameter three
+ * Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
