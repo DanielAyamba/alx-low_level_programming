03cae0480635efc8da203ef51105e92d74c55c5f
DanielAyamba <danielayamba05@gmail.com>
2023-10-23 18:23:56 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _memset - function namee
+  * @b: parameter s
+  * @s: paramater b
+  * @n: parameter n
+  * Return: s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (a = 0; a < n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
