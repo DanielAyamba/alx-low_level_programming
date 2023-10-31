cb945e8bdbf73f03c96a19b6d25621486bbd3013
DanielAyamba <danielayamba05@gmail.com>
2023-10-31 20:14:47 +0000

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,17 @@ char *s;
 
 if (size == 0)
 return (NULL);
+
 s = malloc(size * sizeof(char));
+
 if (s == NULL)
+return (NULL);
+
 for (i = 0; i < size; i++)
 {
 s[i] = c;
 }
+
 return (s);
+
 }
