d4ddd23d3cbb3cee9d35d6ca1d6b12efe1d07def
DanielAyamba <danielayamba05@gmail.com>
2023-11-17 21:51:25 +0000

    deleted 0-print_list.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,26 +3,19 @@
 #include <stdio.h>
 #include "lists.h"
 
+
 /**
-* print_list - check the code for Holberton School students.
+* list_len - check the code for ALX School students.
 * @h: name of the list
 * Return: the number of nodes.
 */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 int count = 0;
 
 
 while (h)
 {
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%d] %s\n", h->len, h->str);
-}
 count++;
 h = h->next;
 }
